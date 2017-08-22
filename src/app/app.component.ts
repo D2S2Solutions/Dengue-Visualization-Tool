5f276a8fd91eccd85e470e62772421da177fb1cf
HeshanSandamal <mhsandamal.13@cse.mrt.ac.lk>
2017-08-22 21:02:13 +0530

    Added multiple graphs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,12 +16,34 @@ export class AppComponent implements AfterViewInit {
     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','January', 'February', 'March', 'April', 'May', 'June', 'July'],
     datasets: [
       {
-        label: 'Dengue Cases',
-        data: [950, 59, 80, 81, 756, 55, 40,950, 59, 80, 81, 756, 55, 40],
+        label: 'ANN',
+        data: [450, 59, 80, 81, 756, 55, 40,350, 59, 80, 81, 656, 55, 40],
         pointBackgroundColor : '#3bd66f',
         borderColor: '#57cc7e',
         pointRadius: 5,
         height: '800',
+        backgroundColor: 'transparent',
+        lineTension: 0
+      },
+      {
+        label: 'Lasso',
+        data: [77, 169, 200, 199, 95, 105, 380,290, 259, 280, 189, 46, 5, 4],
+        pointBackgroundColor : '#f20989',
+        borderColor: '#ef77b9',
+        pointRadius: 5,
+        height: '800',
+        backgroundColor: 'transparent',
+        lineTension: 0
+      },
+      {
+        label: 'Actual',
+        data: [97, 69, 500, 99, 75, 65, 80,90, 59, 180, 89, 76, 45, 49],
+        pointBackgroundColor : '#1b69e5',
+        borderColor: '#8db0e8',
+        pointRadius: 5,
+        height: '800',
+        backgroundColor: 'transparent',
+        lineTension: 0
       }
     ],
     borderColor : [
@@ -33,7 +55,8 @@ export class AppComponent implements AfterViewInit {
   options = {
     backgroundColor: 'red',
     responsive: true,
-    maintainAspectRatio: false
+    maintainAspectRatio: false,
+    bezierCurve : false
   };
 
   ngAfterViewInit() {
